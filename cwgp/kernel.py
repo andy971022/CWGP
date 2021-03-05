8ddd4658c4d372742be2d0a4c788be32f3196a83
andy971022 <andy97_861022_chen@hotmail.com>
2021-03-05 21:14:16 +0900

    1.3.2 matern32 added

--- cwgp/kernel.py
+++ cwgp/kernel.py
@@ -1,11 +1,18 @@
 import numpy as np
 
 
+def distance(x, y, metric="Euclidean"):
+    if metric == "Euclidean":
+        return np.abs(np.subtract.outer(x.reshape(-1), y.reshape(-1)))
+
 def OU(x, y, par=[1]):
     return np.exp(-par[0] *
-                  np.abs(np.subtract.outer(x.reshape(-1), y.reshape(-1))))
+                  distance(x,y))
 
 
 def RBF(x, y, par=[1]):
     return np.exp(-par[0] *
-                  np.abs(np.subtract.outer(x.reshape(-1), y.reshape(-1)))**2)
+                  distance(x,y)**2)
+
+def Matern32(x, y, par=[1,1]):
+    return par[0]*(1+np.sqrt(3)*distance(x,y)/par[1])*np.exp(-np.sqrt(3)*distance(x,y)/par[1])
