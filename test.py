a6dacdde329c8a59ec53aae0064e91f1abd75ed9
andy971022 <andy97_861022_chen@hotmail.com>
2021-04-17 18:42:29 +0900

    1.4.0 grid search test added

--- test.py
+++ test.py
@@ -38,9 +38,20 @@ sns.distplot(betas, ax=ax[1])
 plt.show()
 
 
-def estimator(*args, **kwargs):
-    print(kwargs["test"])
-    pass
+def estimator(**kwargs):
+	print(kwargs)
+	y_train = kwargs["y_train"]
+	y_val = kwargs["y_val"]
+	x_train = kwargs["x_train"]
+	x_val = kwargs["x_val"]
+	for cwgp_model in kwargs["model_holder"]:
+		y_train, y_d = cwgp_model.phi.comp_phi(
+            cwgp_model.phi.res.x, y_train)
+		y_val, y_d = cwgp_model.phi.comp_phi(
+			cwgp_model.phi.res.x, y_val)
+	stats.probplot(y_train, dist="norm", plot=plt)
+	stats.probplot(y_val, dist="norm", plot=plt)
+	plt.show()
 
  # second param is a place holder
  # should give 9^3 combinations
@@ -48,4 +59,4 @@ grid_search(
     estimator, betas, betas, {
         "c": 3, "n": [
             1, 2, 3], "transformations": [
-                "sa", "sal", "box_cox"]}, test="hi")
+                "sa", "sal", "box_cox"]}, test="hi", cv=True,)
