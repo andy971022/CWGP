7669765aaef6f777dd6e12b7602f6afe1ebd76dd
andy971022 <andy97_861022_chen@hotmail.com>
2021-06-11 01:18:19 +0900

    2.0.0 input change

--- cwgp/cwgp.py
+++ cwgp/cwgp.py
@@ -5,8 +5,8 @@ class CWGP():
     def __init__(self, fn, *args, **kwargs):
         self.phi = Phi(fn, *args, **kwargs)
 
-    def fit(self, y, **kwargs):
-        return self.phi.minimize_lf(y, **kwargs)
+    def fit(self, y, t, **kwargs):
+        return self.phi.minimize_lf(y, t, **kwargs)
 
     def normality_test(self):
         pass
