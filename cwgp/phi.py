a37be12137ae3f2b6265fd7cd04d59802b905775
andy971022 <andy97_861022_chen@hotmail.com>
2021-06-24 00:38:52 +0900

    2.0.5 update

--- cwgp/phi.py
+++ cwgp/phi.py
@@ -6,41 +6,44 @@ import GPy
 import itertools
 
 from cwgp.kernel import RBF, OU, Matern32
-from cwgp.transformations import sal, sa, asinh, box_cox, affine, inv_sal, inv_sa, inv_asinh, inv_box_cox, inv_affine
-
+from cwgp.transformations import sal, sa, asinh, box_cox, affine, inv_sal, inv_sa, inv_asinh, inv_box_cox, inv_affine, d_sal, d_sa, d_asinh, d_box_cox, d_affine
 
 class Phi():
     FN_BANK = {
         "sal": {
             "fn": sal,
             "inv_fn": inv_sal,
+            "d_fn": d_sal,
             "par_len": 4,
             "bounds": [
-                (-20,
-                 20)] * 4},
+                (None,
+                 None)] * 4},
         "sa": {
             "fn": sa,
             "inv_fn": inv_sa,
+            "d_fn": d_sa,
             "par_len": 2,
             "bounds": [
-                (-20,
-                 20)] * 2},
+                (None,
+                 None)] * 2},
         "asinh": {
             "fn": asinh,
             "inv_fn": inv_asinh,
+            "d_fn": d_asinh,
             "par_len": 4,
             "bounds": [
-                (-20,
-                 20),
-                (1e-5,
+                (None,
                  None),
                 (1e-5,
                  None),
-                (-20,
-                 20)]},
+                (1e-5,
+                 None),
+                (None,
+                 None)]},
         "box_cox": {
             "fn": box_cox,
             "inv_fn": inv_box_cox,
+            "d_fn": d_box_cox,
             "par_len": 1,
             "bounds": [
                 (1e-5,
@@ -48,6 +51,7 @@ class Phi():
         "affine": {
             "fn": affine,
             "inv_fn": inv_affine,
+            "d_fn": d_affine,
             "par_len": 2,
             "bounds": [
                 (None,
@@ -71,8 +75,10 @@ class Phi():
                    for f in fn]  # a differentiable function
         self.inv_fn = [self.FN_BANK[f]["inv_fn"] for f in fn[::-1]]
         self.par_len = [self.FN_BANK[f]["par_len"] for f in fn]
-        self.d_fn = [elementwise_grad(f, 1)
-                     for f in self.fn]  # take derivative
+        # self.d_fn = [elementwise_grad(f, 1)
+        #              for f in self.fn]  # take derivative
+        self.d_fn = [self.FN_BANK[f]["d_fn"]
+                   for f in fn]
         self.kernel_name = kwargs.get("kernel", "RBF")
         self.kernel = self.KERNEL_BANK[self.kernel_name]["kern"]
         self.kernel_params = self.KERNEL_BANK[self.kernel_name]["params"] if kwargs.get(
@@ -80,14 +86,14 @@ class Phi():
         self.init_scale = self.KERNEL_BANK[self.kernel_name]["init_scale"]
         self.ARD = kwargs.get("ARD", False)
         self.bounds = list(itertools.chain.from_iterable(
-            [self.FN_BANK[f]["bounds"] for f in fn])) + [(None, None)] * self.kernel_params
+            [self.FN_BANK[f]["bounds"] for f in fn])) + [(1e-5, None)] * self.kernel_params
 
     def comp_phi(self, par, y):
         assert len(par) >= sum(self.par_len), "Not enough parameters"
         comp = copy.deepcopy(y)
-        d_comp, par_len = 1, 0
+        d_comp, par_len = 0, 0
         for i in range(0, len(self.fn)):
-            d_comp *= self.d_fn[i](par[par_len:], comp)
+            d_comp += np.log(self.d_fn[i](par[par_len:], comp))
             comp = self.fn[i](par[par_len:], comp)
             par_len += self.par_len[i]
         return comp, d_comp
@@ -104,7 +110,7 @@ class Phi():
         return inv_comp
 
     def likelihood(self, par, y, t, mf):
-        phi_y, chain_d_sal = self.comp_phi(par, y)
+        phi_y, d_phi_y = self.comp_phi(par, y)
         t_phi_y = np.transpose(phi_y)
         t_t = np.transpose(t)
         mean_t = mf(t) if mf else np.zeros(t.shape)
@@ -114,10 +120,13 @@ class Phi():
         else:
             cov_xx = self.kernel(1).K(t.reshape(-1, 1), t.reshape(-1, 1))
 
+
         gaussian_params = 0.5 * \
-            (t_t - np.transpose(mean_t)) @ np.linalg.inv(cov_xx) @ (t - mean_t)
-        return np.ravel(0.5 * np.log(np.linalg.det(cov_xx)) +
-                        gaussian_params - np.sum(np.log(chain_d_sal)))
+            (t_phi_y - np.transpose(mean_t)) @ np.linalg.inv(cov_xx) @ (phi_y - mean_t)
+
+
+        sign, logdet = np.linalg.slogdet(cov_xx)
+        return 0.5 * sign * logdet + gaussian_params - np.sum(d_phi_y)
 
     def reml(self, method):
         pass
@@ -136,7 +145,6 @@ class Phi():
                 try:
                     res = minimize(
                         self.likelihood,
-
                         np.random.uniform(0, self.init_scale,
                                           sum(self.par_len) + self.kernel_params),
                         args=(
